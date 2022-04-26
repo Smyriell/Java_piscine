a15bde85e4a5c966c3aa79688f8c52aa78fbbcbd
Smyriell <78075440+Smyriell@users.noreply.github.com>
2022-04-26 20:19:04 +0300

    ex00

--- Day_05/day05/ex00/Chat/src/main/resources/data.sql
+++ Day_05/day05/ex00/Chat/src/main/resources/data.sql
@@ -1,32 +1,32 @@
 INSERT INTO chat.users (login, password)
-VALUES ('Bob', '000000');
+VALUES ('user1', '111111');
 INSERT INTO chat.users (login, password)
-VALUES ('Billy', '1200000');
+VALUES ('user2', '222222');
 INSERT INTO chat.users (login, password)
-VALUES ('Logan', '000000123');
+VALUES ('user3', '333333');
 INSERT INTO chat.users (login, password)
-VALUES ('Gachi', '000000');
+VALUES ('user4', '444444');
 INSERT INTO chat.users (login, password)
-VALUES ('Andrew', '000000');
+VALUES ('user5', '55555555');
 
 INSERT INTO chat.rooms (name, owner)
-VALUES ('Random', 1);
+VALUES ('General', 1);
 INSERT INTO chat.rooms (name, owner)
-VALUES ('General', 2);
+VALUES ('Special', 2);
 INSERT INTO chat.rooms (name, owner)
-VALUES ('dungeon', 3);
+VALUES ('Jokes', 3);
 INSERT INTO chat.rooms (name, owner)
-VALUES ('Lols', 4);
+VALUES ('Work', 4);
 INSERT INTO chat.rooms (name, owner)
-VALUES ('NMA', 5);
+VALUES ('Hobby', 5);
 
 INSERT INTO chat.messages (author, room, text, timestamp)
-VALUES (1, 1, 'kek', '1970-01-01 00:00:01');
+VALUES (1, 1, 'aaaaaaaaa', '2000-01-01 00:00:01');
 INSERT INTO chat.messages (author, room, text, timestamp)
-VALUES (2, 3, 'lmao', '1970-01-01 00:00:01');
+VALUES (2, 3, 'bbbb', '2000-01-01 00:00:02');
 INSERT INTO chat.messages (author, room, text, timestamp)
-VALUES (5, 3, '300 bucks', '1970-01-01 00:00:02');
+VALUES (5, 3, 'ccccccc', '2000-01-01 00:00:03');
 INSERT INTO chat.messages (author, room, text, timestamp)
-VALUES (4, 4, 'RIPPERONI', '1970-01-01 00:00:04');
+VALUES (4, 4, 'ddddddddd', '2000-01-01 00:00:04');
 INSERT INTO chat.messages (author, room, text, timestamp)
-VALUES (5, 5, 'NMA', '1970-01-01 00:00:05');
+VALUES (5, 5, 'eeeeeeeeee', '2000-01-01 00:00:05');
